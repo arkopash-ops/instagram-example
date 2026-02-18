967bdcd1a6cc5faa2a9e4d468504b726754906de
Aryan <ark.opash@gamil.com>
2026-02-18 12:26:15 +0530

    middlewares added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
+import { loggerMiddleware } from "./middlewares/logger.middleware";
+import { erorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
 
+app.use(loggerMiddleware)
 app.use(cors());
 app.use(express.json());
+app.use(erorMiddleware);
 
 export default app;
